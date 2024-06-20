7a0c2e7102aa25e7affc0a8d2dd0c0e59125a65f
Christian Valderrama <christian@Christians-Mac-Studio.local>
2024-06-20 02:33:55 -0600

    front

--- app/routes/perro.$id.jsx
+++ app/routes/perro.$id.jsx
@@ -21,18 +21,18 @@ import { upsertUsed } from "../data/used.server";
 import { getUserFromSession, updatePuntos } from "../data/auth.server";
 
 const Perro = () => {
-  // const {
-  //   // cuponesEstetica,
-  //   // cuponesGuarderia,
-  //   // cuponesHotel,
-  //   // cuponesAmigos,
-  //   // cuponesDinamicas,
-  //   // cuponesEspeciales,
-  //   peludo,
-  //   user,
-  // } = useLoaderData();
+  const {
+    // cuponesEstetica,
+    // cuponesGuarderia,
+    // cuponesHotel,
+    // cuponesAmigos,
+    // cuponesDinamicas,
+    // cuponesEspeciales,
+    peludo,
+    user,
+  } = useLoaderData();
   //
-  // const navigate = useNavigate();
+  const navigate = useNavigate();
 
   // const actionClose = useActionData();
 
@@ -194,7 +194,7 @@ const Perro = () => {
                   Invitar Amigos{" "}
                 </Link>
               )}
-              <QRCode
+              {/* <QRCode
                 size="120"
                 removeQrCodeBehindLogo="true"
                 logoImage="/logo/lo-vert-JustLikeHome-small-black.png"
@@ -211,7 +211,7 @@ const Perro = () => {
                   [5, 5, 5, 5], // top/right eye
                   [5, 5, 5, 5], // bottom/left
                 ]}
-              />
+              /> */}
             </div>
           </div>
         </div>
