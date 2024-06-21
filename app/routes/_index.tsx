94f687e4afc7c4b064910a82e92843cf78503ca1
Christian Valderrama <christian@Christians-Mac-Studio.local>
2024-06-20 23:27:34 -0600

    front02

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -37,56 +37,58 @@ export default function Index() {
   return (
     <>
       {/* {user?.userId && <BurgerMenu userId={user?.userId} />} */}
-      <div className=" animate-buscador pt-20  flex flex-col items-center content-center justify-center h-screen bg-gray-950 ">
+      <div className=" animate-buscador  flex flex-col items-center content-center justify-center h-screen bg-gray-950 ">
         {/* <Intro /> */}
-        <section className=" -mt-52 flex flex-col gap-2 justify-center items-center">
-          <h2 className="font-montserrat text-2xl ">Bienvenido </h2>
-          <h2 className="font-montserrat text-3xl -my-2">al Programa </h2>
-          <h2 className="font-montserrat text-5xl ">de lealtad </h2>
-          {/* <h1 className="font-montserrat text-4xl font-extrabold">
+        <section className="  flex flex-col gap-2 justify-center items-center">
+          <section className="  flex flex-col gap-2 justify-center items-center">
+            <h2 className="font-montserrat text-2xl ">Bienvenido </h2>
+            <h2 className="font-montserrat text-3xl -my-2">al Programa </h2>
+            <h2 className="font-montserrat text-5xl ">de lealtad </h2>
+            {/* <h1 className="font-montserrat text-4xl font-extrabold">
             Just Like Home
           </h1> */}
-          <img
-            className=" w-11/12 max-w-[400px] mt-7"
-            src={loJLH}
-            alt="logo Just Like Home"
-          />
-          <p className="text-lg">El hub para tus perritos</p>
-        </section>
-        {user?.role === "USER" && (
-          <section className="mt-10 flex flex-col gap-5 items-center">
-            {/* <Link to="/login">
+            <img
+              className=" w-11/12 max-w-[400px] mt-7"
+              src={loJLH}
+              alt="logo Just Like Home"
+            />
+            <p className="text-lg">El hub para tus perritos</p>
+          </section>
+          {user?.role === "USER" && (
+            <section className="mt-10 flex flex-col gap-5 items-center">
+              {/* <Link to="/login">
               ¿Tienes cuenta?{' '}
               <span className=" decoration-solid underline">
                 pasa por aquí.
               </span>
             </Link> */}
-            Mira los cupones de
-            <div className="flex justify-center">
-              <div className="flex ">
-                {perritos.map((perrito) => (
-                  <Link
-                    to={`profile/${perrito.id}`}
-                    key={perrito.id}
-                    className="w-30 h-30 rounded-full overflow-hidden"
-                  >
-                    <img
-                      className=" object-fill w-36 h-36"
-                      alt="perritos"
-                      src={perrito.foto}
-                    />{" "}
-                  </Link>
-                ))}
+              Mira los cupones de
+              <div className="flex justify-center">
+                <div className="flex ">
+                  {perritos.map((perrito) => (
+                    <Link
+                      to={`profile/${perrito.id}`}
+                      key={perrito.id}
+                      className="w-30 h-30 rounded-full overflow-hidden"
+                    >
+                      <img
+                        className=" object-fill w-36 h-36"
+                        alt="perritos"
+                        src={perrito.foto}
+                      />{" "}
+                    </Link>
+                  ))}
+                </div>
               </div>
-            </div>
-            <Link
-              className=" text-center mt-10 flex flex-col gap-5 items-center"
-              to="/login"
-            >
-              <br /> y empieza a recibir beneficios.{" "}
-            </Link>
-          </section>
-        )}
+              <Link
+                className=" text-center mt-10 flex flex-col gap-5 items-center"
+                to="/login"
+              >
+                <br /> y empieza a recibir beneficios.{" "}
+              </Link>
+            </section>
+          )}
+        </section>
 
         {!user?.role && (
           <section className="mt-10 flex flex-col gap-5 items-center">
