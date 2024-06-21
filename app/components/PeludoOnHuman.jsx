c3e8681f3cfd8f92c39d052e5bbfe3020d0bf302
Christian Valderrama <christian@Christians-Mac-Studio.local>
2024-06-20 23:51:19 -0600

    front02

--- app/components/PeludoOnHuman.jsx
+++ app/components/PeludoOnHuman.jsx
@@ -1,25 +1,29 @@
-import { Link } from '@remix-run/react'
+import { Link } from "@remix-run/react";
 
 const PeludoOnHuman = ({ nombre, peludoId, foto, onClick }) => {
   return (
     <div className="pt-6 flex flex-col mt-24 bg-[#37415142]  p-5 rounded-lg">
-      {' '}
-      <div className="rounded-full place-self-center flex w-[130px] bottom h-[130px] bg-gray-700 overflow-hidden -mt-16 mb-10 z-10 justify-center items-stretch">
+      {" "}
+      <Link
+        to={`/perro/${peludoId}`}
+        className="rounded-full place-self-center flex w-[130px] bottom h-[130px] bg-gray-700 overflow-hidden -mt-16 mb-10 z-10 justify-center items-stretch"
+        onClick={onClick}
+      >
         <img
           alt="Foto de tu perrito"
           src={foto}
           className="object-cover w-[130px]  "
         />
-      </div>
+      </Link>
       <Link
-        to={`/profile/${peludoId}`}
+        to={`/perro/${peludoId}`}
         className=" bg-gray-800 text-gray-300 mt-0 py-3 px-5  place-self-center justify-center flex rounded-lg"
         onClick={onClick}
       >
-        Ir al Perfil de{' '}
+        Ir al Perfil de{" "}
         <span className=" first-letter:uppercase font-bold">
-          {' '}
-          &nbsp; {nombre}{' '}
+          {" "}
+          &nbsp; {nombre}{" "}
         </span>
       </Link>
       <Link
@@ -30,7 +34,7 @@ const PeludoOnHuman = ({ nombre, peludoId, foto, onClick }) => {
         Editar
       </Link>
     </div>
-  )
-}
+  );
+};
 
-export default PeludoOnHuman
+export default PeludoOnHuman;
